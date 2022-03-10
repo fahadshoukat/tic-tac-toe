dd192e7cdc3deaf614300a41b9d1f4224a6c65d5
fahadshoukat <fraza6970@gmail.com>
2022-03-10 22:20:00 +0500

    add margin

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,6 +6,7 @@ const boardStyle = {
     borderRadius: '10px',
     width: '250px',
     height: '250px',
+    margin: '0 auto',
     display: 'grid',
     gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
 }
