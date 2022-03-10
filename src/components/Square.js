41d3a5c31c5d6f9a32f98fc3f280d445eb562fed
fahadshoukat <fraza6970@gmail.com>
2022-03-10 22:07:42 +0500

    style squares

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+const squareStyle = {
+    background: 'orange',
+    border: '2px solid lightgreen',
+    fontSize: '30px',
+    fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none'
+}
+
 const Square = ({ onClick, value }) => {
 
-  return <button onClick={onClick}>
+  return <button style={squareStyle} onClick={onClick}>
       {value}
     </button>;
 };
